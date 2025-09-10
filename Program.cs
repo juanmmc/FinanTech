2f246c8ef13860a99548a2a2811f4eefb2f5785b
Juan Miguel Muriel Céspedes <juanmuriel@uagrm.edu.bo>
2025-09-10 15:41:38 -0400

    implementación de patron decorator

--- Program.cs
+++ Program.cs
@@ -8,33 +8,43 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // PDF
-        Console.WriteLine("*** Reporte PDF ***");
-        var pdfBuilder = new PdfReportBuilder();
-        var director = new ReportDirector(pdfBuilder);
-        director.ConstructBasicReport("Reporte Ejecutivo", "Contenido de reporte ejecutivo");
-        var pdfReport = pdfBuilder.Build();
-        var emailDeliveryStrategy = new EmailDeliveryStrategy();
-        emailDeliveryStrategy.Deliver(pdfReport);
-        Console.WriteLine(Environment.NewLine);
+    // PDF
+    Console.WriteLine("*** Reporte Ejecutivo Básico PDF ***");
+    var pdfBuilder = new PdfReportBuilder();
+    var director = new ReportDirector(pdfBuilder);
+    director.ConstructBasicReport("Reporte Ejecutivo", "Contenido de reporte ejecutivo");
+    Report pdfReport = pdfBuilder.Build();
+    // Decoradores: encabezado y marca de agua
+    Report decoratedPdf = new HeaderDecorator(pdfReport, "Confidencial");
+    decoratedPdf = new WatermarkDecorator(decoratedPdf, "FinanTech Solutions");
+    // Entrega por correo
+    var emailDeliveryStrategy = new EmailDeliveryStrategy();
+    emailDeliveryStrategy.Deliver(decoratedPdf);
 
-        // Excel
-        Console.WriteLine("*** Reporte Excel ***");
-        var excelBuilder = new ExcelReportBuilder();
-        director = new ReportDirector(excelBuilder);
-        director.ConstructDetailedReport("Reporte Auditor", "Contenido de reporte auditor", "John Cena", DateTime.Now);
-        var excelReport = excelBuilder.Build();
-        var sharedFolderDeliveryStrategy = new SharedFolderDeliveryStrategy();
-        sharedFolderDeliveryStrategy.Deliver(excelReport);
-        Console.WriteLine(Environment.NewLine);
+    // Excel
+    Console.WriteLine("*** Reporte Auditor Detallado Excel ***");
+    var excelBuilder = new ExcelReportBuilder();
+    director = new ReportDirector(excelBuilder);
+    director.ConstructDetailedReport("Reporte Auditor", "Contenido de reporte auditor", "John Cena", DateTime.Now);
+    Report excelReport = excelBuilder.Build();
+    // Decoradores: marca de agua y compresión
+    Report decoratedExcel = new WatermarkDecorator(excelReport, "FinanTech Solutions");
+    decoratedExcel = new CompressionDecorator(decoratedExcel);
+    // Entrega en carpeta compartida
+    var sharedFolderDeliveryStrategy = new SharedFolderDeliveryStrategy();
+    sharedFolderDeliveryStrategy.Deliver(decoratedExcel);
 
-        // CSV
-        Console.WriteLine("*** Reporte CSV ***");
-        var csvBuilder = new CsvReportBuilder();
-        director = new ReportDirector(csvBuilder);
-        director.ConstructEncryptedReport("Reporte Analista", "Contenido de reporte analista", "asdlkj123");
-        var csvReport = csvBuilder.Build();
-        var apiDeliveryStrategy = new ApiDeliveryStrategy();
-        apiDeliveryStrategy.Deliver(csvReport);
+    // CSV
+    Console.WriteLine("*** Reporte Analista Encriptado CSV ***");
+    var csvBuilder = new CsvReportBuilder();
+    director = new ReportDirector(csvBuilder);
+    director.ConstructEncryptedReport("Reporte Analista", "Contenido de reporte analista", "asdlkj123");
+    Report csvReport = csvBuilder.Build();
+    // Decoradores: encabezado y compresión
+    Report decoratedCsv = new HeaderDecorator(csvReport, "Confidencial");
+    decoratedCsv = new CompressionDecorator(decoratedCsv);
+    // Entrega por API
+    var apiDeliveryStrategy = new ApiDeliveryStrategy();
+    apiDeliveryStrategy.Deliver(decoratedCsv);
     }
 }
